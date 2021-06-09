c72403ee72b4192e8f72a067e7cb646e43fdc124
DKKs <eye.q.21@gmail.com>
2021-06-10 01:43:21 +0700

    [Add] google analytics

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import router from './router'
 import store from './store'
 import jsQR from "jsqr";
 import 'windi.css'
+import VueGtag from "vue-gtag-next";
 
 const app = createApp(App)
 // install any js lib
 app.provide('jsQR', jsQR)
+
+// init google analytic
+app.use(VueGtag, {
+    property: {
+      id: process.env.VUE_APP_GTAG
+    }
+  });
 // init app to #app
 app.use(store).use(router).mount('#app')
