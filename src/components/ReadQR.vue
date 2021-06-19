f0d09fdf62318534bad865a3e5a86eb8cdcad345
DKKs <eye.q.21@gmail.com>
2021-06-20 01:33:19 +0700

    [Add] remove button

--- src/components/ReadQR.vue
+++ src/components/ReadQR.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="h-100vh flex flex-col items-center justify-center p-8">
     <div class="mb-4 text-green-500 text-xl flex items-center"><img src="@/assets/logo-mini.png" class="mr-2" alt="logo"> Open QR</div>
-    <div class="border-dark-800 border-width-2px border-dashed hover:border-green-400 
-    :hover:bg-green-400 p-3 transition-all duration-500"
+    <div class="border-dark-800 border-width-2px border-dashed relative hover:border-green-400 
+    hover:bg-green-100 p-3 transition-all duration-500"
       :class="[file !== null ? 'w-auto h-auto': 'w-full h-full']"
     >
+      <button v-if="file" @click.stop="clear" class="button-clear absolute right-1 top-1 p-3 rounded-full border 
+      bg-white shadow-close-btn hover:bg-warm-gray-200" type="button" >
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#E04F5F">
+          <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+      </button>
       <div class="flex justify-center items-center h-full cursor-pointer" 
       @click.stop="handdleClickUpload"
       >
-         <div v-show="file === null">Click to upload your image qrcode</div>
+         <div v-show="file === null" class="dark:text-danger">Click to upload your image qrcode</div>
          <img v-show="file !== null" class="img" :src="file" alt="alt image" srcset="" />
       </div>
     </div>
     <input ref="uploadinput" class="hidden" type="file" @change="uploadImg" />
     <div class="inline-block mt-4" v-if="link">
-      <a :href="link">{{ link }}</a>
+      <a :href="link" class="hover:transform-gpu hover:font-bold transition-all">{{ link }}</a>
       <div class="m-1">Or</div>
-      <button @click="copyText" class="ml-2 :hover:transform-gpu :hover:scale-75 font-bold">COPY 📑</button>
+      <button @click="copyText" class="ml-2 hover:transform-gpu hover:scale-110 transition-all font-bold">COPY 📑</button>
     </div>
     
     <input class="opacity-0" type="text" name="copy" ref="textToCopy" v-model="link">
@@ -122,7 +128,12 @@ export default {
       window.alert(`Copy 😋`);
     }
 
+    const clear = ()=> {
+      file.value = null
+    }
+
     return {
+      clear,
       copyText,
       textToCopy,
       uploadinput,
